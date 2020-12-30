1c5b5978056dd63267977eea64c760dd5fd3b7c8
hyunwook72 <32571184+hyunwook72@users.noreply.github.com>
2020-12-30 14:16:31 +0900

    데이터형
    bool
    true =! 1
    false =! 0

---
+++ DataType1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class DataType1
+    {
+        static bool BoolVar;
+        static void Main(string[] args)
+        {
+            bool LocalBoolVar = true;
+            Console.WriteLine("{0} {1}", BoolVar, LocalBoolVar);
+
+            Console.ReadKey();
+        }
+    }
+}
