fafb1ca7a0cc3708315b69aa12e09122d0093685
hyunwook72 <32571184+hyunwook72@users.noreply.github.com>
2020-12-30 14:24:45 +0900

    데이터형 : Char형
    유니코드 / 2 byte
    int Number = '7' = 55

---
+++ DataType2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class DataType2
+    {
+        static void Main(string[] args)
+        {
+            char Mun = '7';
+            int Num = Mun;
+            Num = Num + 1;
+            Console.WriteLine("Mun = {0} 문자상수값 = {1} 유니코드문자 = {2}",
+                (int)Mun, Num, (char)Num);
+        }
+    }
+}
