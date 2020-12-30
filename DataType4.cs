ef8ee2087188bd6b55b5b395fb19ae55843f74c9
hyunwook72 <32571184+hyunwook72@users.noreply.github.com>
2020-12-30 15:55:41 +0900

    데이터형 : string 형
    '+' : 문자열 연결
    == : 문자열 비교
    [인덱스] : 문자

---
+++ DataType4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class DataType4
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Hello ";
+            string str2 = "World!";
+            string str3 = str1 + str2;
+            string str4 = "program" + "ming";
+            Console.WriteLine("{0} {1}", str3, str4);
+            Console.WriteLine(str3[4]);
+
+            if (str3 == "Hello World!")
+            {
+                Console.WriteLine("Same");
+            }
+            else
+            {
+                Console.WriteLine("Different");
+            }
+
+            string str5 = "Hello World!";
+            Console.WriteLine("문자 개수: {0}", str5.Length);
+
+            string str6 = "C:\\temp\\test.txt";
+            string str7 = @"c:\temp\text.txt";
+            Console.WriteLine("{0} {1}", str6, str7);
+
+            Console.ReadKey();
+        }
+    }
+}
