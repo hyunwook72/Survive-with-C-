a494726235e201605cba67a0bac21c3f1296f9b8
hyunwook72 <32571184+hyunwook72@users.noreply.github.com>
2020-12-30 19:31:49 +0900

    암시적 데이터형 var
    대입되는 데이터에 따라 데이터형 결정
    
    var를 사용할 수 없는 예 :
     1. null 값 초기화, 매개변수로는 사용 못함
     2. var는 지역변수로만 사용 / 클래스 멤버로는 사용 못함
     3. 연속적으로 초기화 하는 경우 / ex) var m = 10, n = 20;

---
+++ DataType5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class DataType5
+    {
+        static void Main(string[] args)
+        {
+            var value1 = 3.14f; // 3.14f = float , 3.14 = double
+            float value2 = 10.0f;
+            float sum = value1 + value2;
+            Console.WriteLine("{0} {1:f1} {2}", value1, value2, sum);
+            //f1 : 소수점 한자리만 보여줌
+            Console.ReadKey();
+        }
+    }
+}
