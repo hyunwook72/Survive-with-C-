2ab6d5e31456146565f8d9ab9d839820f3b0f8ba
hyunwook72 <32571184+hyunwook72@users.noreply.github.com>
2020-12-30 14:46:29 +0900

    데이터형 : short, ushort 형
    short 유효 범위 : -32768 ~ 32767
    ushort 유효 범위 : 0 ~ 65535

---
+++ DataType3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class DataType3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} ~ {1}", short.MinValue, short.MaxValue);
+
+            Console.ReadKey();
+        }
+    }
+}
