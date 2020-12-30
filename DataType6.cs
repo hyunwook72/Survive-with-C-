65d475f4cf84198a3594a2dcbff3dcdd4a3b2250
hyunwook72 <32571184+hyunwook72@users.noreply.github.com>
2020-12-30 19:51:29 +0900

    데이터형 : nullable 형
    null을 허용하지 않는 데이터형(기본 데이터형)이 null 값을 허용
    형식 : 데이터형? 변수명;
    int? Var1;
    bool? Var2 = null; // true, false, null
    속성 
     .HasValue // true, false
     .Value // 읽기 전용

---
+++ DataType6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class DataType6
+    {
+        static void Main(string[] args)
+        {
+            int? Num1 = null;
+
+            if (Num1.HasValue)
+                Console.WriteLine("Correct Value");
+            else
+                Console.WriteLine("null Value");
+
+            Console.WriteLine("null : {0}", Num1);
+
+            Console.ReadKey();
+        }
+    }
+}
