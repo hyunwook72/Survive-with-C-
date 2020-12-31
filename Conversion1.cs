5466ce9050767091dc939540d4d1f7611c345395
hyunwook72 <32571184+hyunwook72@users.noreply.github.com>
2020-12-31 09:33:43 +0900

    변환 : 데이터 변환
    ToString()
    기본 데이터형.Parse(문자열) // 기본 데이터형 : int, float 등
    Convert.ToInt32() // int
    Convert.ToSingle() // float

---
+++ Conversion1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Conversion1
+    {
+        static void Main(string[] args)
+        {
+            int value1 = 127;
+            string str1 = value1.ToString();
+            Console.WriteLine(str1);
+
+            int value2 = Convert.ToInt32(str1);
+            Console.WriteLine(value2);
+
+            string str2 = "3.14";
+            float value3 = float.Parse(str2);
+            Console.WriteLine(value3);
+
+            Console.ReadKey();
+        }
+    }
+}
