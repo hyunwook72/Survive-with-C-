f17fa9840e75e0bcf6de219bbaa8c90b654ae940
hyunwook72 <32571184+hyunwook72@users.noreply.github.com>
2020-12-31 14:21:55 +0900

    표준 입력1
    ConsoleKeyInfo 속성
     ConsoleKeyInfo.Key
     ConsoleKey 열거형 값
     ConsoleKey.A, ConsoleKey.Escape 등.. // MSDN에서 찾아보기

---
+++ StandardInput1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 사용자가 누를 키를 화면에 출력하되 Escape 키가 입력되면 프로그램을 종료
+namespace ConsoleApp3
+{
+    class StandardInput1
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo KeyInfo;
+            do
+            {
+                KeyInfo = Console.ReadKey();
+                if (KeyInfo.Key == ConsoleKey.A)
+                    Console.WriteLine("You Pressd A"); // 대소문자 구별 못함
+            } while (KeyInfo.Key != ConsoleKey.A);
+
+            Console.ReadKey();
+        }
+    }
+}
