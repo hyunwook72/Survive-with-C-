5d157a8bd32aa83175d32677b446e227147d6a3d
hyunwook72 <32571184+hyunwook72@users.noreply.github.com>
2020-12-31 10:38:21 +0900

    변환 : 박싱과 언박싱
    박싱(boxing) : 데이터 형을 최상위 object 형으로 변환하여 힙(heap 메모리)에 저장
    int m = 123; object obj = m;
    언박싱(unboxing) : 힙에 저장된 형식을 다시 원래의 형식으로 변환
    int n = (int)obj;

---
+++ Conversion2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Conversion2
+    {
+        static void Main(string[] args)
+        {
+            int i = 123;
+            object obj = i;
+            Console.WriteLine("{0}", (int)obj);
+
+            int j = 123;
+            object o = j;
+            j = 456;
+            Console.WriteLine("{0} {1}", j, (int)o);
+
+            Console.ReadKey();
+
+        }
+    }
+}
