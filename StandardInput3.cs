8a7b4705bf43f38aa75301d69004c3b16868afae
hyunwook72 <32571184+hyunwook72@users.noreply.github.com>
2021-01-01 16:09:32 +0900

    표준 입력3
    Console.ReadLine()
    : 엔터키가 눌려질 떄까지 입력 받은 문자열을 리턴하는 메서드
    활용
    : 입력 받은 문자열을 숫자로 사용할 때는 Convert.Toint32() 등의 메서드를 사용

---
+++ StandardInput3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class StandardInput3
+    {
+        static void Main(string[] args)
+        {
+            int Kor, Eng, Math, Total;
+            float Average;
+
+            Console.Write("Korean Score : ");
+            Kor = Convert.ToInt32(Console.ReadLine());
+            Console.Write("English Score : ");
+            Eng = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Math Score : ");
+            Math = Convert.ToInt32(Console.ReadLine());
+            Total = Kor + Eng + Math;
+            Average = Total / 3.0f;
+            Console.WriteLine("{0} {1} {2} {3} {4:f1}", Kor, Eng, Math, Total, Average);
+
+            Console.ReadKey();
+        }
+    }
+}
