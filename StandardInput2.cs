c5329421da611edae641771de1c05ccffba48008
hyunwook72 <32571184+hyunwook72@users.noreply.github.com>
2021-01-01 15:31:19 +0900

    표준 입력2
    ConsoleKeyInfo.KeyChar
    : 눌린 키의 유니코드를 얻는 속성으로 대소문자 등을 모두 구분할 수 있다.

---
+++ StandardInput2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class StandardInput2
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo KeyInfo;
+            do
+            {
+                KeyInfo = Console.ReadKey(true);//true : 타자 보여줌, false : 타자 안 보여줌
+                if (KeyInfo.KeyChar == 'a')
+                    Console.WriteLine("a가 눌렸어");
+                Console.Write(KeyInfo.KeyChar);
+            } while (KeyInfo.Key != ConsoleKey.Escape);
+        }
+    }
+}
